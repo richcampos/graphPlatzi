e9c0db52d56d99dd87f1ccd4da6d770ce33a9ddd
Ricardo Campos <ricamposm@gmail.com>
2019-10-14 18:10:10 -0500

    queries

---
+++ index.js
@@ -0,0 +1,30 @@
+'use strict'
+
+require('dotenv').config()
+const { makeExecutableSchema } = require('graphql-tools')
+const express = require('express')
+const gqlMiddleware = require('express-graphql')
+const { readFileSync } = require('fs')
+const { join } = require('path')
+const resolvers = require('./lib/resolvers')
+
+const app = express()
+const port = process.env.port || 3000
+
+/* Define schema */
+const typeDefs = readFileSync(
+  join(__dirname, 'lib', 'schema.graphql'),
+  'utf-8'
+)
+const schema = makeExecutableSchema({ typeDefs, resolvers })
+
+/** ** ejecutar el query hello */
+app.use('/api', gqlMiddleware({
+  schema: schema,
+  rootValue: resolvers,
+  graphiql: true
+}))
+
+app.listen(port, () => {
+  console.log(`Server is Listening at http://localhost:${port}/api`)
+})
