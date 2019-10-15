9048791234cbfce64f34c68a27bf03daaf2ea013
Ricardo Campos <ricamposm@gmail.com>
2019-10-15 13:05:58 -0500

    production

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 require('dotenv').config()
 const { makeExecutableSchema } = require('graphql-tools')
 const express = require('express')
+const cors = require('cors')
 const gqlMiddleware = require('express-graphql')
 const { readFileSync } = require('fs')
 const { join } = require('path')
@@ -10,6 +11,7 @@ const resolvers = require('./lib/resolvers')
 
 const app = express()
 const port = process.env.port || 3000
+const isDev = process.env.NODE_ENV !== 'production'
 
 /* Define schema */
 const typeDefs = readFileSync(
@@ -18,11 +20,13 @@ const typeDefs = readFileSync(
 )
 const schema = makeExecutableSchema({ typeDefs, resolvers })
 
-/** ** ejecutar el query hello */
+app.use(cors())
+
+/* ejecutar el query hello */
 app.use('/api', gqlMiddleware({
   schema: schema,
   rootValue: resolvers,
-  graphiql: true
+  graphiql: isDev
 }))
 
 app.listen(port, () => {
